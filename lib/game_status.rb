dfd95085c16991808ba3b769dd14faaf3f80ed44
Andrew C <acchang4@gmail.com>
2020-04-10 21:38:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,7 +12,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
 #  returns falsey for an empty board (FAILED - 1)
+#  returns falsey for a draw
 if board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+if board = !WIN_COMBINATIONS
 false
 end
 end
