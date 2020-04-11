5c0db12fcf58e75a2306035cd6b28cb6ed62299b
Andrew C <acchang4@gmail.com>
2020-04-11 02:54:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,6 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-
 def won?(board)
   WIN_COMBINATIONS.each do |win_combo|
     index_0 = win_combo[0]
@@ -43,24 +42,3 @@ end
 #  returns an array of matching indexes for a right column win
 #  returns an array of matching indexes for a left diagonal win
 #  returns an array of matching indexes for a right diagonal win
-
-
-   |win_combo|"
-end
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
-  end
-
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
-    end
-  }
-  return false
-end
